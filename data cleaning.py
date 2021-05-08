0991b42d49617551603e5efe80ea0f3f7dd102e2
anudeep-dasari <dasarianudeep.22@gmail.com>
2021-05-08 19:25:36 +0530

    reinstated orignal salary column

--- data cleaning.py
+++ data cleaning.py
@@ -13,21 +13,22 @@ df = df[df['Salary Estimate'] != '-1']
 df = df.reset_index(drop=True)
 
 #cleaning Salary column
-df['Salary Estimate'] = df['Salary Estimate'].str.split('(').str[0]
+df['Salary'] = df['Salary Estimate'].str.split('(').str[0]
 
-df['Salary Estimate'] = df['Salary Estimate'].str.replace('$','')
-df['Salary Estimate'] = df['Salary Estimate'].str.replace('K','')
-df['Salary Estimate'] = df['Salary Estimate'].str.replace('Employer Provided Salary:','')
+df['Salary'] = df['Salary'].str.replace('$','')
+df['Salary'] = df['Salary'].str.replace('K','')
+df['Salary'] = df['Salary'].str.replace('Employer Provided Salary:','')
 
-df['hourly'] = df['Salary Estimate'].str.contains('Per Hour',regex=False).astype(int)
-df['Salary Estimate'] = df['Salary Estimate'].str.replace('Per Hour','')
+df['hourly'] = df['Salary'].str.contains('Per Hour',regex=False).astype(int)
+df['Salary'] = df['Salary'].str.replace('Per Hour','')
 
-df['Salary Estimate'] = df['Salary Estimate'].str.split('-')
+df['Salary'] = df['Salary'].str.split('-')
 
-df['Minimum Salary'] = df['Salary Estimate'].str[0].astype(int)
-df['Maximum Salary'] = df['Salary Estimate'].str[1].astype(int)
+df['Minimum Salary'] = df['Salary'].str[0].astype(int)
+df['Maximum Salary'] = df['Salary'].str[1].astype(int)
 
 df['Average Salary'] = (df['Minimum Salary']+df['Maximum Salary'])/2
+df = df.drop(['Salary'],axis=1)
 
 #Getting Company Name and rating from Company name column
 df['Company'] = df.apply(lambda x: x['Company Name'] if x['Rating']<0 else x['Company Name'][:-3], axis=1)
